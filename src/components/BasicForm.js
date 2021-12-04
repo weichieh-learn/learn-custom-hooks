1a73f3292001ad6cf2554bc484cb5758cee3cb8d
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-12-04 12:52:22 +0800

    basic form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,25 +1,102 @@
+import useInput from '../hooks/use-input'
+
 const BasicForm = (props) => {
+  const {
+    value: enteredFName,
+    isValid: enteredFNameIsValid,
+    hasError: fnameHasError,
+    valueChangeHandler: fnameChangeHandler,
+    inputBlurHandler: fnameBlurHandler,
+    reset: resetFNameInput,
+  } = useInput((value) => value.trim() !== '')
+
+  const {
+    value: enteredLName,
+    isValid: enteredLNameIsValid,
+    hasError: lnameHasError,
+    valueChangeHandler: lnameChangeHandler,
+    inputBlurHandler: lnameBlurHandler,
+    reset: resetLNameInput,
+  } = useInput((value) => value.trim() !== '')
+
+  let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.trim().match(pattern))
+
+  let formIsValid = false
+  if (enteredFNameIsValid && enteredLNameIsValid && enteredEmailIsValid) {
+    formIsValid = true
+  }
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault()
+
+    if (!enteredFNameIsValid || !enteredLNameIsValid || !enteredEmailIsValid) {
+      return
+    }
+    resetFNameInput()
+    resetLNameInput()
+    resetEmailInput()
+  }
+
+  const fnameClasses = fnameHasError ? 'form-control invalid' : 'form-control'
+  const lnameClasses = fnameHasError ? 'form-control invalid' : 'form-control'
+  const emailClasses = emailHasError ? 'form-control invalid' : 'form-control'
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="control-group">
+        <div className={fnameClasses}>
+          <label htmlFor="fname">First Name</label>
+          <input
+            type="text"
+            id="fname"
+            onChange={fnameChangeHandler}
+            onBlur={fnameBlurHandler}
+            value={enteredFName}
+          />
+          {fnameHasError && (
+            <p className="error-text">First Name must not be empty.</p>
+          )}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lnameClasses}>
+          <label htmlFor="lname">Last Name</label>
+          <input
+            type="text"
+            id="lname"
+            onChange={lnameChangeHandler}
+            onBlur={lnameBlurHandler}
+            value={enteredLName}
+          />
+          {lnameHasError && (
+            <p className="error-text">Last Name must not be empty.</p>
+          )}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailClasses}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+        {emailHasError && (
+          <p className="error-text">Please enter a valid email.</p>
+        )}
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default BasicForm;
+export default BasicForm
